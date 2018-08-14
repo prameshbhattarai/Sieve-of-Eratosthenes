ac37fa40542f05d3f66f5a4573ae41b27772af67
prameshbhattarai <pra4mesh@gmail.com>
2018-08-14 20:56:22 +0545

    Remove unused import.

--- src/sieve/Main.java
+++ src/sieve/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         SieveOfEratosthenes s = new SieveOfEratosthenes(1000_000_00);
         MultiThreadSieveOfEratosthenes m = new MultiThreadSieveOfEratosthenes(1000_000_00, 10);
+        AtomicSieveOfEratosthenes a = new AtomicSieveOfEratosthenes(1000_000_00, 10);
 
         Long start = System.nanoTime();
         System.gc();
@@ -12,13 +13,20 @@ public class Main {
         Long end = System.nanoTime();
         System.out.println("Time taken " + (end - start));
 
-        Long mstart = System.nanoTime();
+        start = System.nanoTime();
         System.gc();
         boolean[] mBookKeeping = m.process();
-        Long mend = System.nanoTime();
-        System.out.println("Time taken " + (mend - mstart));
+        end = System.nanoTime();
+        System.out.println("Time taken " + (end - start));
+
+//        start = System.nanoTime();
+//        System.gc();
+//        boolean[] aBookKeeping = a.process();
+//        end = System.nanoTime();
+//        System.out.println("Time taken " + (end - start));
 
         System.out.println("Size of primes calculated by SieveOfEratosthenes " + Util.sizeOfPrime(bookKeeping));
         System.out.println("Size of primes calculated by MultiThreadSieveOfEratosthenes " + Util.sizeOfPrime(mBookKeeping));
+//        System.out.println("Size of primes calculated by AtomicThreadSieveOfEratosthenes " + Util.sizeOfPrime(aBookKeeping));
     }
 }
--- src/sieve/MultiThreadSieveOfEratosthenes.java
+++ src/sieve/MultiThreadSieveOfEratosthenes.java
@@ -3,7 +3,6 @@ package sieve;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MultiThreadSieveOfEratosthenes {
